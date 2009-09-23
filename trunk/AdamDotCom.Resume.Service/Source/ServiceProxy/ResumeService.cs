09d9b60d9eade171f0cbf6408d6d1a84dd3e8667
kahtava <kahtava@97abc296-a169-11de-be8d-735280eae207>
2009-09-23 17:33:08 +0000

    Added service proxy and integration tests

---
+++ trunk/AdamDotCom.Resume.Service/Source/ServiceProxy/ResumeService.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.Serialization;
+using System.ServiceModel;
+
+[assembly: ContractNamespace("http://adam.kahtava.com/services/resume", ClrNamespace = "AdamDotCom.Resume.Service.Proxy")]
+namespace AdamDotCom.Resume.Service.Proxy
+{
+    public class ResumeService: ClientBase<IResume>, IResume
+    {
+        public Resume ResumeXml(string firstnameLastname)
+        {
+            return base.Channel.ResumeXml(firstnameLastname);
+        }
+
+        public Resume ResumeJson(string firstnameLastname)
+        {
+            return base.Channel.ResumeJson(firstnameLastname);
+        }
+    }
+}
