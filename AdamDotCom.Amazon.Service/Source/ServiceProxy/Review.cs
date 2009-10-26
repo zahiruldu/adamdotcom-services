451442f379bcff19acce7d6c3f8b912b772a3f11
kahtava <kahtava@97abc296-a169-11de-be8d-735280eae207>
2009-10-26 03:14:04 +0000

    Added Review to proxy.

---
+++ AdamDotCom.Amazon.Service/Source/ServiceProxy/Review.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace AdamDotCom.Amazon.Service.Proxy
+{
+    public class Review
+    {
+        public string Summary  { get; set; }
+
+        public string Content  { get; set; }
+
+        public DateTime Date  { get; set; }
+
+        public int HelpfulVotes  { get; set; }
+
+        public int TotalVotes  { get; set; }
+
+        public decimal Rating  { get; set; }
+
+        public string ASIN  { get; set; }
+
+        public string Title  { get; set; }
+
+        public string AuthorsMLA  { get; set; }
+
+        public string Authors  { get; set; }
+
+        public string Url  { get; set; }
+
+        public string ImageUrl  { get; set; }
+
+        public string Publisher  { get; set; }
+
+        public string ProductPreviewUrl  { get; set; }
+    }
+}
