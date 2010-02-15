3ac3b138887b36c82f2669fe4ac99ac54aacfd22
adam.kahtava.com <adam.kahtava.com@97abc296-a169-11de-be8d-735280eae207>
2010-02-15 05:58:15 +0000

    Patched a bug where the the cache hash was reflectivly refering to the wrong object

---
+++ trunk/AdamDotCom.Common.Service/Source/Unit.Tests/ServiceCacheTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using AdamDotCom.Common.Service;
+using NUnit.Framework;
+
+namespace Unit.Tests
+{
+    [TestFixture]
+    public class ServiceCacheTests
+    {
+        public class MyTestObject
+        {
+        }
+
+        [Test]
+        public void Test()
+        {
+            var hash = ServiceCache.Hash<MyTestObject>("dude");
+
+            Assert.IsTrue(hash.Contains("MyTestObject"));
+            Assert.IsTrue(hash.Contains("dude"));
+            Console.Write(hash);
+        }
+    }
+}
