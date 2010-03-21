09c3a8b771ffad8d919ca988cd380dc9efc0b2bc
adam.kahtava.com <adam.kahtava.com@97abc296-a169-11de-be8d-735280eae207>
2010-03-21 16:00:56 +0000

    Added conditional caching for my objects, broke JSONP stuff into a seperate assembly

---
+++ trunk/AdamDotCom.Common.Service/Source/Common/Utilities/StringUtilities.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AdamDotCom.Common.Service.Utilities
+{
+    public static class StringUtilities
+    {
+        public static string Scrub(string dirtyString)
+        {
+            if (!string.IsNullOrEmpty(dirtyString))
+            {
+                return dirtyString.Replace("\r\n", "").Replace("\n", "").Replace("\r", "").Replace("\t", "").Replace("     ", "").Replace(",,", ",").Trim();
+            }
+            return dirtyString;
+        }
+
+        public static List<string> Scrub(List<string> list)
+        {
+            if (list == null)
+            {
+                return list;
+            }
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (list[i] == null)
+                {
+                    list.RemoveAt(i);
+                }
+                else
+                {
+                    list[i] = Scrub(list[i]);
+                }
+            }
+            return list;
+        }
+    
+        public static bool Has(this string value, string query)
+        {
+            return value.IndexOf(query, StringComparison.CurrentCultureIgnoreCase) != -1;
+        }
+    }
+}
---
+++ trunk/AdamDotCom.Common.Service/Source/Unit.Tests/StringUtilitiesTests.cs
@@ -0,0 +1,16 @@
+﻿using AdamDotCom.Common.Service.Utilities;
+using NUnit.Framework;
+
+namespace Unit.Tests
+{
+    [TestFixture]
+    public class StringUtilitiesTests
+    {
+        [Test]
+        public void Test()
+        {
+            Assert.IsTrue("twist-twist-flip-twist-kahtava".Has("KAHTAVA"));
+            Assert.IsFalse("twist-twist-flip-twist-kahtava".Has("umpa-lumpa"));
+        }
+    }
+}
