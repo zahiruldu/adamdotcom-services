36326b5012ec14db7a00198ab1b79dc98ebd4373
adam.kahtava.com <adam.kahtava.com@97abc296-a169-11de-be8d-735280eae207>
2010-05-18 00:27:00 +0000

    First cut of CSV Behavior for WCF

---
+++ trunk/AdamDotCom.Common.Service/Source/Common/Infrastructure/CSV/XmlToCvsTranslator.cs
@@ -0,0 +1,41 @@
+﻿using System.IO;
+using System.Reflection;
+using System.Xml;
+using System.Xml.Xsl;
+
+namespace AdamDotCom.Common.Service.Infrastructure.CSV
+{
+    public class XmlToCvsTranslator
+    {
+        private XmlDocument xslt;
+
+        public XmlToCvsTranslator()
+        {
+            LoadXslt();
+        }
+
+        public StreamWriter Translate(XmlReader contents, StreamWriter writer)
+        {
+            LoadXslt();
+            var transform = new XslCompiledTransform();
+            transform.Load(xslt);
+
+            transform.Transform(contents, null, writer);
+            
+            return writer;
+        }
+
+        private void LoadXslt()
+        {
+            xslt = new XmlDocument();
+
+            var fileWithNamespace = string.Format("{0}.{1}", GetType().Namespace, "XmlToCsv.xslt");
+            var fileStream = Assembly.GetExecutingAssembly().GetManifestResourceStream(fileWithNamespace);
+
+            using (var reader = new StreamReader(fileStream))
+            {
+                xslt.LoadXml(reader.ReadToEnd());
+            }
+        }
+    }
+}
---
+++ trunk/AdamDotCom.Common.Service/Source/Common/Infrastructure/JSONP/JSONPEncoder.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.ServiceModel.Channels;
+using System.IO;
+using System.Xml;
+using System.Runtime.Serialization.Json;
+
+namespace AdamDotCom.Common.Service.Infrastructure.JSONP
+{
+    public class JSONPEncoder
+    {
+        public ArraySegment<byte> WriteMessage(MessageEncoder encoder, Message message, int maxMessageSize, BufferManager bufferManager, int messageOffset)
+        {
+            MemoryStream stream = new MemoryStream();
+            StreamWriter sw = new StreamWriter(stream);
+
+            string methodName = null;
+            if (message.Properties.ContainsKey(JSONPBehavior.Name))
+            {
+                methodName = ((JSONPMessageProperty) (message.Properties[JSONPBehavior.Name])).MethodName;
+            }
+
+            if (methodName != null)
+            {
+                sw.Write(methodName + "( ");
+                sw.Flush();
+            }
+
+            XmlWriter writer = JsonReaderWriterFactory.CreateJsonWriter(stream);
+            message.WriteMessage(writer);
+            writer.Flush();
+            if (methodName != null)
+            {
+                sw.Write(" );");
+                sw.Flush();
+            }
+
+            byte[] messageBytes = stream.GetBuffer();
+            int messageLength = (int) stream.Position;
+            int totalLength = messageLength + messageOffset;
+            byte[] totalBytes = bufferManager.TakeBuffer(totalLength);
+            Array.Copy(messageBytes, 0, totalBytes, messageOffset, messageLength);
+
+            ArraySegment<byte> byteArray = new ArraySegment<byte>(totalBytes, messageOffset, messageLength);
+            writer.Close();
+
+            return byteArray;
+        }
+    }
+}
