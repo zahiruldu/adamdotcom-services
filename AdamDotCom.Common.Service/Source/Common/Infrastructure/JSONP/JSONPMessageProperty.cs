26604339180e51b6c6052332cb0ad738fa7aacad
adam.kahtava.com <adam.kahtava.com@97abc296-a169-11de-be8d-735280eae207>
2010-05-18 01:01:40 +0000

    New assembly with both CSV and JSONP WCF Behaviors

---
+++ AdamDotCom.Common.Service/Source/Common/Infrastructure/JSONP/JSONPMessageProperty.cs
@@ -0,0 +1,23 @@
+﻿using System.ServiceModel.Channels;
+
+namespace AdamDotCom.Common.Service.Infrastructure.JSONP
+{
+    public class JSONPMessageProperty : IMessageProperty
+    {
+        public IMessageProperty CreateCopy()
+        {
+            return new JSONPMessageProperty(this);
+        }
+
+        public JSONPMessageProperty()
+        {
+        }
+
+        internal JSONPMessageProperty(JSONPMessageProperty other)
+        {
+            MethodName = other.MethodName;
+        }
+
+        public string MethodName { get; set; }
+    }
+}
